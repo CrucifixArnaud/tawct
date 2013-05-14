e360a345ad1bc8f116e65020f8d1286ff628e836
Arnaud Crucifix <acr@mb-acr.home>
2013-05-14 17:20:59 +0200

    Reduce Img Size

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
 ?>
 
 <!doctype html>
-<!--[if lt IE 7]>	<html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>		<html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>		<html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]><html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]><html class="lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--><html lang="en"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
-	<title></title>
+	<title>The Amazing Wc Tour</title>
 	<meta name="author" content="Crucifix Arnaud">
-	<meta name="description" content="">
+	<meta name="description" content="WC that I have visited in my life. Simple photography, no pictures improvement, just raw shoots of toilets, takes in every day's life condition">
 	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="ui/css/styles.css">	  
+	<link rel="stylesheet" href="ui/css/styles.css">  
 </head>
 <body>
 	<div id="global" class="no-js">
