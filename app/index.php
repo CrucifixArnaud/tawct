714ea4b0b1fb556372c1b59665b245ac7a3cdfe5
acr <hello@crucifixarnaud.com>
2016-12-27 14:24:56 +0100

    Only lte IE8 for ie condionnal comment

--- app/index.php
+++ app/index.php
@@ -6,11 +6,7 @@
   $picturesCount = 0;
 ?>
 <!doctype html>
-<!--[if lt IE 7]> <html class="ie ie6 lte9 lte8 lte7" lang="en"> <![endif]-->
-<!--[if IE 7]> <html class="ie ie7 lte9 lte8 lte7" lang="en"> <![endif]-->
-<!--[if IE 8]> <html class="ie ie8 lte9 lte8" lang="en"> <![endif]-->
-<!--[if IE 9]> <html class="ie ie9 lte9" lang="en"> <![endif]-->
-<!--[if gt IE 9]> <html class="ie" lang="en"> <![endif]-->
+<!--[if lte IE 8]> <html class="no-js lt-e8" lang="en"> <![endif]-->
 <!--[if !IE]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
